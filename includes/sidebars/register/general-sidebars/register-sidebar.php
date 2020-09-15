ade978ea8aceff091eb52acd80574073c1d73ea2
truvoice <miky@local.com>
2020-09-15 03:18:03 +0100

    added nav sidebar

--- includes/sidebars/register/general-sidebars/register-sidebar.php
+++ includes/sidebars/register/general-sidebars/register-sidebar.php
@@ -37,6 +37,18 @@ function tru_fetcher_register_sidebars() {
     );
     register_sidebar( $topBar );
 
+    $navBar = array(
+        'name'          =>  __( 'Nav Bar' ),
+        'id'            => "nav-bar",
+        'description'   => '',
+        'class'         => '',
+        'before_widget' => '<li id="%1$s" class="widget %2$s">',
+        'after_widget'  => "</li>\n",
+        'before_title'  => '<h2 class="widgettitle">',
+        'after_title'   => "</h2>\n",
+    );
+    register_sidebar( $navBar );
+
     $footer = array(
         'name'          =>  __( 'Footer' ),
         'id'            => "footer",
